e6e8556a4871388d96364ad916715af6460c27d3
duckhee <fain9301@yahoo.com>
2017-04-15 15:38:18 +0900

    mac commit

--- calender/webserver.js
+++ calender/webserver.js
@@ -4,7 +4,15 @@ var http = require('http') ;
 var app = express() ;
 
 
+
 app.set('port', process.env.PORT || 5000) ;
+
+app.use('/', function(request, response, next) {
+
+	
+
+} ) ;
+
 
 
 http.createServer.listen(app.get('port'), function(){
